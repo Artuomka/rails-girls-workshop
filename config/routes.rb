67be72cfd7dd37f8f8735e1362968797a0713cd1
Igor Shkidchenko <igorshkidchenko@gmail.com>
2019-09-02 12:07:37 +0300

    Remove stubs from routes and remove controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,2 @@
 Rails.application.routes.draw do
-  if Rails.env.development? || Rails.env.staging?
-    get 'stubs/:template', to: 'stubs#show', as: 'stubs_template'
-    get 'stubs/:folder/:template', to: 'stubs#show', as: 'nested_stubs_template'
-    get 'stubs/:layout/:folder/:template', to: 'stubs#show', as: 'layout_nested_stubs_template'
-  end
-
-  root to: 'stubs#index'
 end
